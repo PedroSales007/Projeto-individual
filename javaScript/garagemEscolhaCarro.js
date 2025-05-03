9979b2f1c030725635371213cc493c3f2354835b
PedroSales <pedro.santos013@sptech.school>
2025-05-02 22:56:20 -0300

    Adicionado toda parte de estilo e animaçõa da garagem de carro

---
+++ javaScript/garagemEscolhaCarro.js
@@ -0,0 +1,227 @@
+
+marcaCarro.innerHTML = `<img src="../imagens/ferrari/logo-ferrari.png" alt="">`
+    valorCR.innerHTML = "8000.000"
+
+    nomeMarca.innerHTML = "Ferrari"
+    nomeModelo.innerHTML = "F40"
+    modeloAno.innerHTML = "1987"
+    classe.innerHTML = "SUPER CARS"
+
+    imgCarro.innerHTML = `<img src="../imagens/ferrari/f40/f40.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/s-632.png" alt="">`
+
+    legendaTracao.innerHTML = "RWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/tracao-trazeira.png" alt="">`
+
+    velocidade.innerHTML = "8.0"
+    nivelPorcentagemVelocidade.style.width = "120px"
+
+    controle.innerHTML = "9.4"
+    nivelPorcentagemControle.style.width = "141px"
+
+    aceleracao.innerHTML = "8.3"
+    nivelPorcentagemAceleracao.style.width = "124.5px"
+
+    arrancada.innerHTML = "7.8"
+    nivelPorcentagemArrancagem.style.width = "117px"
+
+    frenagem.innerHTML = "9.2"
+    nivelPorcentagemFrenagem.style.width = "138px"
+function ferrarif40(){
+    marcaCarro.innerHTML = `<img src="../imagens/ferrari/logo-ferrari.png" alt="">`
+    valorCR.innerHTML = "8000.000"
+
+    nomeMarca.innerHTML = "Ferrari"
+    nomeModelo.innerHTML = "F40"
+    modeloAno.innerHTML = "1987"
+    classe.innerHTML = "SUPER CARS"
+
+    imgCarro.innerHTML = `<img src="../imagens/ferrari/f40/f40.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/s-632.png" alt="">`
+
+    legendaTracao.innerHTML = "RWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/tracao-trazeira.png" alt="">`
+
+    velocidade.innerHTML = "8.0"
+    nivelPorcentagemVelocidade.style.width = "120px"
+
+    controle.innerHTML = "9.4"
+    nivelPorcentagemControle.style.width = "141px"
+
+    aceleracao.innerHTML = "8.3"
+    nivelPorcentagemAceleracao.style.width = "124.5px"
+
+    arrancada.innerHTML = "7.8"
+    nivelPorcentagemArrancagem.style.width = "117px"
+
+    frenagem.innerHTML = "9.2"
+    nivelPorcentagemFrenagem.style.width = "138px"
+
+}
+
+function srtViper(){
+    marcaCarro.innerHTML = `<img src="../imagens/viper/viper-logo.png" alt=""></img>`
+    valorCR.innerHTML = "110.000"
+
+    nomeMarca.innerHTML = "Viper"
+    nomeModelo.innerHTML = "GTS"
+    modeloAno.innerHTML = "2013"
+    classe.innerHTML = "SUPER CARS"
+
+    imgCarro.innerHTML = `<img src="../imagens/viper/viper-gts.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/r3-800.png" alt="">`
+
+    legendaTracao.innerHTML = "RWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/tracao-trazeira.png" alt="">`
+
+    velocidade.innerHTML = "9.0"
+    nivelPorcentagemVelocidade.style.width = "135px"
+
+    controle.innerHTML = "9.6"
+    nivelPorcentagemControle.style.width = "144px"
+
+    aceleracao.innerHTML = "8.4"
+    nivelPorcentagemAceleracao.style.width = "126px"
+
+    arrancada.innerHTML = "7.5"
+    nivelPorcentagemArrancagem.style.width = "112.5px"
+
+    frenagem.innerHTML = "9.5"
+    nivelPorcentagemFrenagem.style.width = "145.5px"
+
+}
+
+function nissanSkyline(){
+    marcaCarro.innerHTML = `<img src="../imagens/nissan/logo-gtr.png" alt=""></img>`
+    valorCR.innerHTML = "52.000"
+
+    nomeMarca.innerHTML = "Nissan "
+    nomeModelo.innerHTML = "Skyline GT-R"
+    modeloAno.innerHTML = "2000"
+    classe.innerHTML = "SPORTS CAR"
+
+    imgCarro.innerHTML = `<img src="../imagens/nissan/nissan-gtr.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/b-461.png" alt="">`
+
+    legendaTracao.innerHTML = "AWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/4x4.png" alt="">`
+
+    velocidade.innerHTML = "6.1"
+    nivelPorcentagemVelocidade.style.width = "91.5px"
+
+    controle.innerHTML = "8.5"
+    nivelPorcentagemControle.style.width = "127.5px"
+
+    aceleracao.innerHTML = "6.8"
+    nivelPorcentagemAceleracao.style.width = "102px"
+
+    arrancada.innerHTML = "7.4"
+    nivelPorcentagemArrancagem.style.width = "111px"
+
+    frenagem.innerHTML = "8.3"
+    nivelPorcentagemFrenagem.style.width = "124.5px"
+
+}
+
+function bugattiVeyron(){
+    marcaCarro.innerHTML = `<img src="../imagens/bugatti/logo-bugatti.png" alt=""></img>`
+    valorCR.innerHTML = "2.200.000"
+
+    nomeMarca.innerHTML = "Bugatti"
+    nomeModelo.innerHTML = "Veyron Super Sport"
+    modeloAno.innerHTML = "2011"
+    classe.innerHTML = "SUPER CARS"
+
+    imgCarro.innerHTML = `<img src="../imagens/bugatti/bugatti-veyron.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/r2-900.png" alt="">`
+
+   legendaTracao.innerHTML = "AWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/4x4.png" alt="">`
+
+    velocidade.innerHTML = "10"
+    nivelPorcentagemVelocidade.style.width = "150px"
+
+    controle.innerHTML = "9.4"
+    nivelPorcentagemControle.style.width = "141px"
+
+    aceleracao.innerHTML = "10"
+    nivelPorcentagemAceleracao.style.width = "150px"
+
+    arrancada.innerHTML = "9.8"
+    nivelPorcentagemArrancagem.style.width = "147px"
+
+    frenagem.innerHTML = "9.4"
+    nivelPorcentagemFrenagem.style.width = "141px"
+
+}
+
+function toyotaSupraRZ(){
+    marcaCarro.innerHTML = `<img src="../imagens/toyotaSupra/logo-supra.png" alt="">`
+    valorCR.innerHTML = "8000.00"
+
+    nomeMarca.innerHTML = "Toyota"
+    nomeModelo.innerHTML = "Supra RZ"
+    modeloAno.innerHTML = "2013"
+    classe.innerHTML = "SPORTS CAR"
+
+    imgCarro.innerHTML = `<img src="../imagens/toyotaSupra/supra-rz.png" alt="" width="300px">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/b-464.png" alt="">`
+
+    legendaTracao.innerHTML = "RWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/tracao-trazeira.png" alt="">`
+
+    velocidade.innerHTML = "6.8"
+    nivelPorcentagemVelocidade.style.width = "102px"
+
+    controle.innerHTML = "8.3"
+    nivelPorcentagemControle.style.width = "124.5px"
+
+    aceleracao.innerHTML = "6.8"
+    nivelPorcentagemAceleracao.style.width = "102px"
+
+    arrancada.innerHTML = "6.6"
+    nivelPorcentagemArrancagem.style.width = "99px"
+
+    frenagem.innerHTML = "8.0"
+    nivelPorcentagemFrenagem.style.width = "120px"
+
+}
+
+function lamborghiniMurcielago(){
+    marcaCarro.innerHTML = `<img src="../imagens/lamborghini/logo-lamborghini.png" alt=""></img>`
+    valorCR.innerHTML = "8000.00"
+
+    nomeMarca.innerHTML = "Lamborghini"
+    nomeModelo.innerHTML = "Murciélago"
+    modeloAno.innerHTML = "2010"
+    classe.innerHTML = "SUPER CARS"
+
+    imgCarro.innerHTML = `<img src="../imagens/lamborghini/lambo-murcielago.png" alt="">`
+
+    nivel.innerHTML = `<img src="../imagens/classe-tracao/niveis/s-700.png" alt="">`
+
+    legendaTracao.innerHTML = "AWD"
+    imgTracao.innerHTML = `<img src="../imagens/classe-tracao/4x4.png" alt="">`
+
+    velocidade.innerHTML = "9.1"
+    nivelPorcentagemVelocidade.style.width = "136.5px"
+
+    controle.innerHTML = "9.3"
+    nivelPorcentagemControle.style.width = "139.5px"
+
+    aceleracao.innerHTML = "8.6"
+    nivelPorcentagemAceleracao.style.width = "129px"
+
+    arrancada.innerHTML = "7.9"
+    nivelPorcentagemArrancagem.style.width = "118.5px"
+
+    frenagem.innerHTML = "9.3"
+    nivelPorcentagemFrenagem.style.width = "139.5px"
+
+}
