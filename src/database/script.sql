ab2e11dee9eb72f6e1d45720f67ab79d013a6506
PedroSales <pedro.santos013@sptech.school>
2025-05-07 13:50:25 -0300

    Tentando conexão com a API web-data-viz

---
+++ src/database/script.sql
@@ -0,0 +1,10 @@
+create database horizonTuner;
+
+use horizonTuner;
+
+create table cadastro(
+idCadastro int primary key auto_increment,
+nome varchar(50),
+email varchar(50),
+senha varchar(50)
+);
